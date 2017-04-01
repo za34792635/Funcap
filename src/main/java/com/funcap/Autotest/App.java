011824a88c73965c2253059be7565294b80630ad
duncan <380366595@qq.com>
2017-04-01 12:01:48 +0800

    Maven项目自动化项目

---
+++ src/main/java/com/funcap/Autotest/App.java
@@ -0,0 +1,13 @@
+package com.funcap.Autotest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
